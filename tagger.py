6e38338e75ab248a279fb87f1bd6550a85267a80
Anna Bettina Ovsiyovna Steinberg <steinberga@cip.ifi.lmu.de>
2019-01-17 09:22:17 +0100

    Argparse implemented

--- tagger.py
+++ tagger.py
@@ -11,37 +11,43 @@ from fofe_prep_class import DataPrep
 from fofe_model import FOFE_Encoding, FOFE_GRU
 from classic_model import Classic_GRU
 from sklearn.metrics import f1_score
+import time
+import argparse
 
 
 ###### MAIN PROCESSING #############
 
 class Tagger:
 
-    def __init__(self, modelname, datafile, batchsize, embeddingsize, hiddensize, numepochs):
+    def __init__(self, args):
         vocab_char_size = len(['<PAD>'] + sorted(string.printable))
 
-        self.data = DataPrep(datafile, batchsize, modelname)
+        self.data = DataPrep(args.datafile, args.batch_size, args.modelname)
         numlabels = len(self.data.label_to_id)
         vocabsize = len(self.data.word_to_id)
 
-        self.train(modelname, vocabsize, vocab_char_size, embeddingsize,
-                   hiddensize, numepochs, numlabels)
+        self.train(args.modelname, vocabsize, vocab_char_size, args.embedding_size,
+                   args.hidden_size, args.num_epochs, numlabels, args.learn_rate, args.reg_factor)
 
     def print(self, model):
         for name, param in model.named_parameters():
             if name == "fofe.forgetting_factor":
                 print(name, param.data)
 
-    def train(self, modelname, vocab_size, vocab_char_size, embedding_size, hidden_size, num_epochs, num_labels):
+    def train(self, modelname, vocab_size, vocab_char_size, embedding_size, hidden_size, num_epochs, num_labels, learn_rate, reg_factor):
+        begin = time.time()
+        print(begin)
         if modelname == "FOFE":
             model = FOFE_GRU(vocab_char_size, hidden_size, num_labels)
         elif modelname == "Classic":
             model = Classic_GRU(vocab_size, embedding_size,
                                 hidden_size, num_labels)
+        # if torch.cuda.is_available:
+            # model.cuda()
         # to ignore padded_elements in loss calculation
         criterion = nn.CrossEntropyLoss(ignore_index=0)
         optimizer = optim.Adam(model.parameters(),
-                               lr=0.0001, weight_decay=0.001)
+                               lr=learn_rate, weight_decay=reg_factor)
         best_acc = 0
         best_f1_macro = 0
         best_f1_micro = 0
@@ -50,6 +56,9 @@ class Tagger:
             print(epoch)
             loss_accum = 0.0
             for batch, (labels, lengths) in zip(self.data.train_input, self.data.train_labels):
+                # if torch.cuda._isavailable():
+                    #batch = batch.cuda()
+                    #labels = labels.cuda()
                 optimizer.zero_grad()
                 output = model.forward(batch)
                 loss = criterion(output, labels)
@@ -58,9 +67,12 @@ class Tagger:
                 optimizer.step()    # Does the update
             print("train loss:", loss_accum/len(self.data.train_input))
             self.print(model)
-            if epoch % 2 == 0:
+            if epoch % 100 == 0:
                 dev_loss_accum = 0.0
                 for batch, (labels, lengths) in zip(self.data.dev_input, self.data.dev_labels):
+                    # if torch.cuda._isavailable():
+                        #batch = batch.cuda()
+                        #labels = labels.cuda()
                     output_dev = model.forward(batch)
                     loss_dev = criterion(output_dev, labels)
                     dev_loss_accum += loss_dev.data.item()
@@ -76,6 +88,11 @@ class Tagger:
                         print("f1_micro", best_f1_micro)
 
                 print("dev loss", dev_loss_accum/len(self.data.dev_input))
+                print("acc", best_acc)
+                print("f1 macro", best_f1_macro)
+                print("f1 micro", best_f1_micro)
+        end = time.time() - begin
+        print(end)
 
     def eval(self, output, act_labels):
         _, pred_labels = output.max(dim=1)
@@ -99,7 +116,25 @@ class Tagger:
         return acc, f1_macro, f1_micro
 
 
-Tagger("FOFE", datafile="Atis.json", batchsize=8,
-       embeddingsize=100, hiddensize=100, numepochs=1000)
-# Tagger("Classic", datafile="Tiger/", batchsize=8,
-# embeddingsize=100, hiddensize=100, numepochs=10)
+parser = argparse.ArgumentParser(
+    description='Training program of the BIOS Tagger.')
+parser.add_argument('modelname', type=str,
+                    help='type of model to be trained: FOFE encodings or Classic embeddings')
+parser.add_argument('datafile', type=str,
+                    help='file or folder containing the data')
+parser.add_argument('--batch_size', type=int, default=8,
+                    help='size of the data batches')
+parser.add_argument('--embedding_size', type=int, default=100,
+                    help='size of the word embeddings when using ')
+parser.add_argument('--hidden_size', type=int, default=100,
+                    help='size of hidden states of GRU')
+parser.add_argument('--num_epochs', type=int, default=1000,
+                    help='number of epochs used for training')
+parser.add_argument('--learn_rate', type=float,
+                    default=0.001, help='learning rate for model')
+parser.add_argument('--reg_factor', type=float, default=0.001,
+                    help='weight decay factor of model')
+
+args = parser.parse_args()
+
+Tagger(args)
