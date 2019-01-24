ec1129a9dc184eb69c3c47377601bcc66d1fe56e
Anna Bettina Ovsiyovna Steinberg <steinberga@cip.ifi.lmu.de>
2019-01-24 17:38:10 +0100

    Random shuffling included

--- train_tagger_MB.sh
+++ train_tagger_MB.sh
@@ -7,7 +7,7 @@ num_epochs=($(seq 0 50 550))
 learn_rate=0.001
 reg_factor=0.0
 
-./tagger_cuda.py FOFE Atis.json \
+python3.6 tagger_cuda.py FOFE Atis.json \
 --batch_size=$batch_size \
 --embedding_size=$embedding_size \
 --hidden_size=$hidden_size \
--- wrapper.py
+++ wrapper.py
@@ -3,19 +3,23 @@ from tagger_cuda import Tagger
 
 class Wrapper:
 
-    def __init__(self, modelname, datafile):
+    def __init__(self, modelname, datafile, batch_size):
         self.modelname = modelname
         self.datafile = datafile
+        self.batchsize = batch_size
 
     # config_dict={param: value_choice, ....}
     def learn(self, num_epochs, config_dict, seed):
         model = Tagger(self.modelname, self.datafile,
-                       num_epochs, config_dict)
+                       num_epochs, self.batchsize, **config_dict)
         # train
-        metrics = model.train(num_epochs, config_dict)
-
-        return metric(=accuracy)
+        metrics = model.train(num_epochs, seed, **config_dict)
+        # metrics is dict = {epoch: (model, train_loss, dev_loss,test_loss, acc, f1_macro, f1_weighted)}
+        return metrics
 
 
 if __name__ == '__main__':
-    learner = Wrapper('FOFE', 'Atis.json')
+    learner = Wrapper('FOFE', 'Atis.json', 8)
+    config_dict = {'embedding_size': 100, 'hidden_size': 100,
+                   'dropout_rate': 0.5, 'learn_rate': 0.01, 'reg_factor': 0.0}
+    learner.learn([0, 1, 5, 10], config_dict, 0)
